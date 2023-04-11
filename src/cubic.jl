c55f52ad00fe6cd1ff44cf3bb9992904012fb4e7
singularitti <singularitti@outlook.com>
2023-04-11 08:43:59 -0400

    Fix src/cubic.jl

--- src/cubic.jl
+++ src/cubic.jl
@@ -1,6 +1,7 @@
 c11, c12, c44 = symbols("c11 c12 c44")
+cj = [c11, c12, c44]
 ϵ₁, ϵ₂, ϵ₃, ϵ₄, ϵ₅, ϵ₆ = symbols("ϵ₁ ϵ₂ ϵ₃ ϵ₄ ϵ₅ ϵ₆")
-const ϵ = [ϵ₁, ϵ₂, ϵ₃, ϵ₄, ϵ₅, ϵ₆]
+ϵ = [ϵ₁, ϵ₂, ϵ₃, ϵ₄, ϵ₅, ϵ₆]
 c = [
     c11 c12 c12 0 0 0
     0 c11 c12 0 0 0
@@ -11,5 +12,4 @@ c = [
 ]
 c = Symmetric(c)
 u = elastic_energy(c, ϵ)
-cj = [c11, c12, c44]
-s_matrix = Sij(u, cj)
+s_matrix = Sij(u, cj, ϵ)
