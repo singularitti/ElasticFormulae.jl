ab3c77c7b35b87389b745b49319640cdc9cdbd2c
singularitti <singularitti@outlook.com>
2023-04-11 08:02:10 -0400

    Update src/ElasticFormulae.jl

--- src/ElasticFormulae.jl
+++ src/ElasticFormulae.jl
@@ -1,5 +1,23 @@
 module ElasticFormulae
 
-# Write your package code here.
+using LinearAlgebra: UpperTriangular, dot
+using SymEngine
+
+export symmetrize, elastic_energy, sij, Sij
+
+elastic_energy(c, ϵ) = expand(dot(ϵ, c, ϵ) / 2)
+
+sij(u::Basic, ϵᵢ::Basic, cⱼ::Basic) = diff(diff(u, ϵᵢ), cⱼ)
+
+function Sij(u::Basic, c, ϵ)
+    n = length(c)
+    result = Matrix{Basic}(undef, 6, n)
+    for i in 1:6
+        for j in 1:n
+            result[i, j] = sij(u, ϵ[i], c[j])
+        end
+    end
+    return result
+end
 
 end
